61dfac75aa658a55f469e7bd24e36d3cc653b090
Ольга Петрова <luchezarnaya79olga@yandex.ru>
2022-09-19 15:41:10 +0300

    Код отформатирован

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
         int ticketPrice = 1350; // стоимость билета
 
-        int amountMile=20; // количество рублей на 1 милю
+        int amountMile = 20; // количество рублей на 1 милю
         int bonusMile = 0; // количество бонусных миль
 
-        System.out.println("Бонусные мили =" + (bonusMile=ticketPrice/amountMile));
+
+        System.out.println("Стоимость билета =" + ticketPrice);
+        System.out.println("Количество рублей за 1 милю =" + amountMile);
+        System.out.println("Бонусные мили =" + (bonusMile = ticketPrice / amountMile));
 
     }
 
